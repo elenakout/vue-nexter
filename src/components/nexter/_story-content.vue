8911f8f1e80c27c264689e4f8cc64c5b0c749216
elenakout <elenakout@gmail.com>
2019-07-11 18:32:44 +0300

    7.Building the Story Section - Part 2

--- src/components/nexter/_story-content.vue
+++ src/components/nexter/_story-content.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="story__content-comp">
-    Story Content
+    <h3 class="heading-3 mb-sm">Happy Customers</h3>
+    <h2 class="heading-2 heading-2--dark mb-md">
+      &ldquo;The best decision of our lives&rdquo;
+    </h2>
+    <p class="story__text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, quas
+      praesentium labore temporibus commodi quidem. Soluta, asperiores!
+    </p>
+    <button class="btn">Find your own home</button>
   </div>
 </template>
 
@@ -8,4 +16,12 @@
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.story {
+  &__text {
+    font-size: 1.5rem;
+    font-style: italic;
+    margin-bottom: 4rem;
+  }
+}
+</style>
