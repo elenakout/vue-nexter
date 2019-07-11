ee265f393b0b43ea84d1bf415708aa307469ce78
elenakout <elenakout@gmail.com>
2019-07-11 14:24:02 +0300

    vue-transform

--- src/components/nexter/_features.vue
+++ src/components/nexter/_features.vue
@@ -1,73 +1,60 @@
 <template>
   <div class="features-comp">
-    <div class="feature">
+    <div class="feature" v-for="(item, idx) in features" :key="idx">
       <svg class="feature__icon">
-        <use xlink:href="../../assets/img/sprite.svg#icon-global"></use>
+        <use href="../../assets/img/sprite.svg#icon-global"></use>
       </svg>
-      <h4 class="heading-4">World's best luxury homes</h4>
+      <h4 class="heading-4">{{ item.heading }}</h4>
       <p class="feature__text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio officiis
-        voluptates repudiandae quos.
-      </p>
-    </div>
-
-    <div class="feature">
-      <svg class="feature__icon">
-        <use xlink:href="../../assets/img/sprite.svg#icon-trophy"></use>
-      </svg>
-      <h4 class="heading-4">Only the best properties</h4>
-      <p class="feature__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rem
-        accusantium dolores ratione minima?
-      </p>
-    </div>
-
-    <div class="feature">
-      <svg class="feature__icon">
-        <use xlink:href="../../assets/img/sprite.svg#icon-map-pin"></use>
-      </svg>
-      <h4 class="heading-4">All homes in top locations</h4>
-      <p class="feature__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
-    </div>
-
-    <div class="feature">
-      <svg class="feature__icon">
-        <use xlink:href="../../assets/img/sprite.svg#icon-key"></use>
-      </svg>
-      <h4 class="heading-4">New home in one week</h4>
-      <p class="feature__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium id
-        doloribus fuga officia asperiores?
-      </p>
-    </div>
-
-    <div class="feature">
-      <svg class="feature__icon">
-        <use xlink:href="../../assets/img/sprite.svg#icon-presentation"></use>
-      </svg>
-      <h4 class="heading-4">Top 1% realtors</h4>
-      <p class="feature__text">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, aliquid
-        aliquam.
-      </p>
-    </div>
-
-    <div class="feature">
-      <svg class="feature__icon">
-        <use xlink:href="../../assets/img/sprite.svg#icon-lock"></use>
-      </svg>
-      <h4 class="heading-4">Secure payments on nexter</h4>
-      <p class="feature__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        {{ item.text }}
       </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      features: [
+        {
+          heading: "World's best luxury homes",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio officiis voluptates repudiandae quos.",
+          icon: "../../assets/img/sprite.svg#icon-global"
+        },
+        {
+          heading: "Only the best properties",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rem accusantium dolores ratione minima?",
+          icon: "../../assets/img/sprite.svg#icon-trophy"
+        },
+        {
+          heading: "All homes in top locations",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          icon: "../../assets/img/sprite.svg#icon-map-pin"
+        },
+        {
+          heading: "New home in one week",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium id doloribus fuga officia asperiores?",
+          icon: "../../assets/img/sprite.svg#icon-key"
+        },
+        {
+          heading: "Top 1% realtors",
+          text:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, aliquid aliquam.",
+          icon: "../../assets/img/sprite.svg#icon-presentation"
+        },
+        {
+          heading: "Secure payments on nexter",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          icon: "../../assets/img/sprite.svg#icon-lock"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss">
