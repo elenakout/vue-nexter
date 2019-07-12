b0650219fd036ea1b3970302585fc6f915e89c3d
elenakout <elenakout@gmail.com>
2019-07-12 20:41:45 +0300

    13.building the sidebar

--- src/components/nexter/_sidebar.vue
+++ src/components/nexter/_sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sidebar-comp">
-    Sidebar
+    <button class="nav-btn"></button>
   </div>
 </template>
 
@@ -8,4 +8,35 @@
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.sidebar-comp {
+  display: flex;
+  justify-content: center;
+}
+
+.nav-btn {
+  border: none;
+  border-radius: 0;
+
+  background-color: #fff;
+  height: 2px;
+  width: 4.5rem;
+  margin-top: 4rem;
+
+  &::before,
+  &::after {
+    background-color: #fff;
+    height: 2px;
+    width: 4.5rem;
+    content: "";
+    display: block;
+  }
+
+  &::before {
+    transform: translateY(-1.5rem);
+  }
+  &::after {
+    transform: translateY(1.3rem);
+  }
+}
+</style>
